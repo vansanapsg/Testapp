447209ca74ee0338813b3c08f28df89f396dce2a
Bobby <vansanag@gmail.com>
2016-12-13 11:41:32 +0700

    Initial commit

---
+++ app/src/main/java/psg/bobby/testapp/MainActivity.java
@@ -0,0 +1,13 @@
+package psg.bobby.testapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
