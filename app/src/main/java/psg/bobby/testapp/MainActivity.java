290aca9aefc05a83efe7407931c0a5c7392dfdf3
Bobby <vansanag@gmail.com>
2016-12-13 14:43:16 +0700

    no actionbar finish start coding

--- app/src/main/java/psg/bobby/testapp/MainActivity.java
+++ app/src/main/java/psg/bobby/testapp/MainActivity.java
@@ -1,7 +1,9 @@
 package psg.bobby.testapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    } // main method
+
+    public void clickSignUpMain(View view) {
+        startActivity(new Intent(MainActivity.this, SignUpActivity.class));
     }
-}
+
+} // main class
