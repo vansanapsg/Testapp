8bbdd5c0ab24837fdc863d90e6bdd8cf425fed6d
Bobby <vansanag@gmail.com>
2016-12-13 16:56:27 +0700

    finish 13dec

--- app/src/main/java/psg/bobby/testapp/SignUpActivity.java
+++ app/src/main/java/psg/bobby/testapp/SignUpActivity.java
@@ -2,12 +2,46 @@ package psg.bobby.testapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 public class SignUpActivity extends AppCompatActivity {
+
+
+    //explicit
+    private EditText nameEditText, userEditText, passwordEditText;
+    private String nameString, userString, passwordString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
-    }
-}
+
+        // bind widget
+        nameEditText = (EditText) findViewById(R.id.editText);
+        userEditText = (EditText) findViewById(R.id.editText2);
+        passwordEditText = (EditText) findViewById(R.id.editText3);
+
+
+
+    } // Main method
+
+    public void clickSignUpSign(View view) {
+
+        nameString = nameEditText.getText().toString().trim();
+        userString=userEditText.getText().toString().trim();
+        passwordString = passwordEditText.getText().toString().trim();
+
+        // check space
+        if (nameString.equals("") || userString.equals("") || passwordString.equals("")) {
+
+            //have space
+            Log.d("13decV1", "Have space");
+
+        }
+
+    } //clickSign
+
+
+} // Main class
