74b76ff36bef3fd5cddde9a9d2d8f5640c961197
Bobby <vansanag@gmail.com>
2016-12-14 17:04:10 +0700

    finish 14dec

--- app/src/main/java/psg/bobby/testapp/MainActivity.java
+++ app/src/main/java/psg/bobby/testapp/MainActivity.java
@@ -3,15 +3,21 @@ package psg.bobby.testapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
     //explicit
     private EditText userEditText, passwordEditText;
     private String userString, passwordString;
-
+    private String[] loginStrings;
+    private boolean aBoolean = true;
 
 
     @Override
@@ -28,6 +34,8 @@ public class MainActivity extends AppCompatActivity {
 
     public void clickSignInMain(View view) {
 
+
+
         userString = userEditText.getText().toString().trim();
         passwordString = passwordEditText.getText().toString().trim();
 
@@ -35,13 +43,74 @@ public class MainActivity extends AppCompatActivity {
         if (userString.equals("") || passwordString.equals(""))
         {
             //have space
-            //Myalert myalert=new Myalert(MainActivity.this,)
+            Myalert myalert=new Myalert(MainActivity.this,
+                    getResources().getString(R.string.title_have_space),
+                    getResources().getString(R.string.message_have_space),
+                    R.drawable.bird48);
+            myalert.myDialog();
+            Log.d("xxx", userString);
+
 
 
 
         } else
+            Log.d("xxx", "false");
         {
             //no space
+            try {
+
+                SyncUser syncUser = new SyncUser(MainActivity.this);
+                syncUser.execute();
+                String s = syncUser.get();
+                Log.d("14decV2", "JSON ==>" + s);
+
+
+                JSONArray jsonArray = new JSONArray(s);
+
+                loginStrings = new String[4];
+                for (int i=0; i<jsonArray.length(); i+=1) {
+
+                    JSONObject jsonObject = jsonArray.getJSONObject(i);
+
+                    if (userString.equals(jsonObject.get("User"))) {
+
+                        loginStrings[0] = jsonObject.getString("id");
+                        loginStrings[1] = jsonObject.getString("Name");
+                        loginStrings[2] = jsonObject.getString("User");
+                        loginStrings[3] = jsonObject.getString("Password");
+
+                        aBoolean = false;
+
+                    }
+
+                } // for
+
+                if (aBoolean) {
+
+                    Myalert myalert = new Myalert(MainActivity.this,
+                            getResources().getString(R.string.title_user_false),
+                            getResources().getString(R.string.message_user_false),
+                            R.drawable.rat48);
+                } else if (passwordString.equals(loginStrings[3])) {
+                    //password true
+                    Toast.makeText(MainActivity.this, "welcome " + loginStrings[1],
+                            Toast.LENGTH_SHORT).show();
+                } else
+                {
+                    //password false
+                    Myalert myalert = new Myalert(MainActivity.this,
+                            getResources().getString(R.string.title_pass_false),
+                            getResources().getString(R.string.message_pass_false),
+                            R.drawable.kon48);
+                    myalert.myDialog();
+
+                }
+
+            } catch (Exception e) {
+
+                Log.d("14decV2", "e Main ==>" + e.toString());
+
+            }
 
 
         }
@@ -50,6 +119,7 @@ public class MainActivity extends AppCompatActivity {
     } // click sign In
 
     public void clickSignUpMain(View view) {
+        //Toast.makeText(MainActivity.this, "click", Toast.LENGTH_SHORT);
         startActivity(new Intent(MainActivity.this, SignUpActivity.class));
     }
 
---
+++ app/src/main/java/psg/bobby/testapp/SyncUser.java
@@ -0,0 +1,45 @@
+package psg.bobby.testapp;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.util.Log;
+
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+/**
+ * Created by Bobby on 12/14/2016.
+ */
+
+public class SyncUser extends AsyncTask<Void, Void, String>
+{
+    //explicit
+    private Context context;
+    private static final String urlJSON = "http://swiftcodingthai.com/ltc/php_get_data_bobby.php";
+
+    public SyncUser(Context context)
+    {
+        this.context = context;
+    }
+
+    @Override
+    protected String doInBackground(Void... params) {
+
+        try {
+            OkHttpClient okHttpClient = new OkHttpClient();
+            Request.Builder builder = new Request.Builder();
+            Request request = builder.url(urlJSON).build();
+            Response response = okHttpClient.newCall(request).execute();
+            return response.body().string();
+
+
+
+
+        } catch (Exception e) {
+
+            Log.d("14decV2", "e doin ==> " + e.toString());
+        }
+        return null;
+    }
+} // Main class
