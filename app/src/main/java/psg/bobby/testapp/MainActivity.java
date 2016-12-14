f2e618d72a64e7871bb64e997bfae132e1501aae
Bobby <vansanag@gmail.com>
2016-12-14 15:23:49 +0700

    design ui main

--- app/src/main/java/psg/bobby/testapp/MainActivity.java
+++ app/src/main/java/psg/bobby/testapp/MainActivity.java
@@ -4,14 +4,50 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    //explicit
+    private EditText userEditText, passwordEditText;
+    private String userString, passwordString;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //bind widget
+        userEditText = (EditText) findViewById(R.id.editText4);
+        passwordEditText = (EditText) findViewById(R.id.editText5);
+
+
     } // main method
+
+    public void clickSignInMain(View view) {
+
+        userString = userEditText.getText().toString().trim();
+        passwordString = passwordEditText.getText().toString().trim();
+
+
+        if (userString.equals("") || passwordString.equals(""))
+        {
+            //have space
+            //Myalert myalert=new Myalert(MainActivity.this,)
+
+
+
+        } else
+        {
+            //no space
+
+
+        }
+
+
+    } // click sign In
 
     public void clickSignUpMain(View view) {
         startActivity(new Intent(MainActivity.this, SignUpActivity.class));
