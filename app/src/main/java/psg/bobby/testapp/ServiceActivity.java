f63828a5a0b00045cecdc8a9bae8876ed995d913
Bobby <vansanag@gmail.com>
2016-12-15 16:42:29 +0700

    pending gps

--- app/src/main/java/psg/bobby/testapp/ServiceActivity.java
+++ app/src/main/java/psg/bobby/testapp/ServiceActivity.java
@@ -6,11 +6,18 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.widget.TextViewCompat;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -22,12 +29,23 @@ public class ServiceActivity extends FragmentActivity implements OnMapReadyCallb
     private GoogleMap mMap;
     private LocationManager locationManager;
     private Criteria criteria;
-    private double latADouble, lngADouble;
+    private double latADouble, lngADouble, updatelatADouble, updatelngADouble;
+    private String[] loginStrings;
+    private TextView textView;
+    private EditText editText;
+    private ImageView imageView, takephotoImageView;
+    private String nameImageString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.my_layout);
+
+        //bind widget
+        textView = (TextView) findViewById(R.id.textView5);
+        editText = (EditText) findViewById(R.id.editText6);
+        imageView = (ImageView) findViewById(R.id.imageView2);
+        takephotoImageView = (ImageView) findViewById(R.id.imageButton);
 
         // setup
         locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
@@ -35,6 +53,10 @@ public class ServiceActivity extends FragmentActivity implements OnMapReadyCallb
         criteria.setAccuracy(Criteria.ACCURACY_FINE);
         criteria.setAltitudeRequired(false);
         criteria.setBearingRequired(false);
+        loginStrings = getIntent().getStringArrayExtra("Login");
+
+        //show
+        textView.setText(loginStrings[1]);
 
 
 
@@ -43,6 +65,59 @@ public class ServiceActivity extends FragmentActivity implements OnMapReadyCallb
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     } // Main method
+
+    public void clickSave(View view) {
+
+        nameImageString = editText.getText().toString().trim();
+
+        //check space
+        if (nameImageString.equals("")) {
+            //move space
+            Myalert myalert = new Myalert(ServiceActivity.this,
+                    getResources().getString(R.string.title_have_space),
+                    getResources().getString(R.string.message_have_space),
+                    R.drawable.doremon48);
+            myalert.myDialog();
+
+        }
+
+    } //click save
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        latADouble = 17.969686;
+        lngADouble = 102.612177;
+
+        Location networkLocation = myFindLocation(LocationManager.NETWORK_PROVIDER);
+        if (networkLocation != null) {
+
+            latADouble = networkLocation.getLatitude();
+            lngADouble = networkLocation.getLongitude();
+
+        }
+
+        Location gpsLocation = myFindLocation(LocationManager.GPS_PROVIDER);
+        if (gpsLocation != null) {
+
+            latADouble = gpsLocation.getLatitude();
+            lngADouble = gpsLocation.getLongitude();
+
+        }
+
+        Log.d("15decV1", "lat ==>" + latADouble);
+        Log.d("15decV1", "lng ==>" + lngADouble);
+
+    } // on resume
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        locationManager.removeUpdates(locationListener);
+    }
 
     public Location myFindLocation(String strProvider) {
 
@@ -91,9 +166,34 @@ public class ServiceActivity extends FragmentActivity implements OnMapReadyCallb
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+        LatLng latLng = new LatLng(latADouble, lngADouble);
+
+
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 16));
+
+        myAddMarker(latADouble, lngADouble);
+
+        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+                mMap.clear();
+                myAddMarker(latLng.latitude, latLng.longitude);
+            }
+        });
+
     } //on Map
+
+    private void myAddMarker(double latADouble, double lngADouble) {
+
+        LatLng latLng = new LatLng(latADouble, lngADouble);
+        mMap.addMarker(new MarkerOptions().position(latLng)
+        .icon(BitmapDescriptorFactory.fromResource(R.drawable.build3)));
+
+        updatelatADouble = latADouble;
+        updatelngADouble = lngADouble;
+        Log.d("15decV2", "updatelat ==>" + updatelatADouble);
+        Log.d("15decV2", "updatelng ==>" + updatelngADouble);
+
+    }
 } // Main class
